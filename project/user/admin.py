930ea12632fb92f5adb61e26459f6fcbd4691e5c
Valentin <valenp97@gmail.com>
2022-03-28 21:09:52 +0200

    Fixed mistake in admin.py

--- project/user/admin.py
+++ project/user/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import CustomUser, CustomUserManager
+from .models import CustomUser
 
-admin.site.register(CustomUser, CustomUserManager)
+admin.site.register(CustomUser)

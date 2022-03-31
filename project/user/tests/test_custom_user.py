b07b697618c9bfe0dbb9cecc3b28230cac6a8435
Valentin <valenp97@gmail.com>
2022-03-31 19:51:51 +0200

    Changes to test_custom_user code; first draft of PrivateUserTest for testing JWT authentication
    Prior changes refer to switching django.test.TestCase for rest_framework.test.APITestCase as it is more appropiate and offers more functionalities.
    The switch was uncomplicated as I think APITestCase inherits from TestCase.
    Also changed the register base url to match the previous commit of changing Create to Register.

--- project/user/tests/test_custom_user.py
+++ project/user/tests/test_custom_user.py
@@ -1,14 +1,15 @@
-from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 
 
-CREATE_USER_URL = reverse('user:create')
+REGISTER_USER_URL = reverse('user:register')
+JWT_OBTAIN_URL = reverse('user:token_obtain_pair')
+JWT_REFRESH_URL = reverse('user:token_refresh_pair')
 
 
-class PublicUserTest(TestCase):
+class PublicUserTest(APITestCase):
     """
     Test CustomUser functionality prior to authentication.
     """
@@ -23,7 +24,7 @@ class PublicUserTest(TestCase):
             'name': 'John Doe',
             'password': 'testpass123'
         }
-        response = self.client.post(CREATE_USER_URL, payload)
+        response = self.client.post(REGISTER_USER_URL, payload)
 
         # Firstly test that the object was created in the serverside
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -43,10 +44,10 @@ class PublicUserTest(TestCase):
             'password': 'testpass123'
         }
         # Create user with the defined payload
-        self.client.post(CREATE_USER_URL, payload)
+        self.client.post(REGISTER_USER_URL, payload)
 
         # Now attempt to create another user with the same payload and capture the response
-        response = self.client.post(CREATE_USER_URL, payload)
+        response = self.client.post(REGISTER_USER_URL, payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -58,7 +59,7 @@ class PublicUserTest(TestCase):
             'password': 'short1'
         }
         # Capture the response from the user creation attempt
-        response = self.client.post(CREATE_USER_URL, payload)
+        response = self.client.post(REGISTER_USER_URL, payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -79,7 +80,7 @@ class PublicUserTest(TestCase):
             'password': 'abcdefghijk'
         }
         # Capture the response from the user creation attempt
-        response = self.client.post(CREATE_USER_URL, payload)
+        response = self.client.post(REGISTER_USER_URL, payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -97,7 +98,7 @@ class PublicUserTest(TestCase):
             'password': 'my password36'
         }
         # Capture the response from the user creation attempt
-        response = self.client.post(CREATE_USER_URL, payload)
+        response = self.client.post(REGISTER_USER_URL, payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -106,3 +107,43 @@ class PublicUserTest(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+
+class PrivateUserTest(APITestCase):
+    """
+    Test for authenticated users with JWT
+    """
+
+    def setUp(self):
+        """Register a test user to use in each test"""
+        self.user = get_user_model().objects.create_user(
+            email='test@email.com',
+            name='Test Johnson',
+            password='testpass876'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)  # TODO: investigate; does this line make sense with JWT?
+
+    def test_user_success_jwt(self):
+        """Test that a JWT is generated"""
+        user = {
+            'email': self.user.get('email'),
+            'name': self.user.get('name'),
+            'password': self.user.get('password'),
+        }
+
+        response = self.client.post(JWT_OBTAIN_URL, data=user)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+    def test_refresh_jwt(self):
+        """Test that a new JWT is generated"""
+        response = self.client.post(JWT_OBTAIN_URL, data=self.user, format='json')
+
+        jwt = response.data.get('token', None)
+
+        if jwt:
+            response = self.client.post(JWT_REFRESH_URL, data=jwt, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertIn('token', response.data)
